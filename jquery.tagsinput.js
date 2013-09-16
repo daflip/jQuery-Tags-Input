b419c95184b8c6852ed5bc09bd5f4e2b00fd2828
Jerry Walsh <jerry.walsh@gmail.com>
2013-09-16 16:16:06 +0100

    add placeholder attrinput on input element for modern browsers

--- jquery.tagsinput.js
+++ jquery.tagsinput.js
@@ -221,7 +221,7 @@
 			var markup = '<div id="'+id+'_tagsinput" class="tagsinput"><div id="'+id+'_addTag">';
 			
 			if (settings.interactive) {
-				markup = markup + '<input id="'+id+'_tag" value="" data-default="'+settings.defaultText+'" />';
+				markup = markup + '<input id="'+id+'_tag" value="" placeholder="'+settings.defaultText+'" data-default="'+settings.defaultText+'" />';
 			}
 			
 			markup = markup + '</div><div class="tags_clear"></div></div>';
